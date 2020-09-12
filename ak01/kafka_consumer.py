cf18caa44cc8a1d9a81347a5e6e68118b933cadf
Ｍartin Cho <66872857+marx1992620@users.noreply.github.com>
2020-09-12 13:39:03 +0800

    Update kafka_consumer.py

--- ak01/kafka_consumer.py
+++ ak01/kafka_consumer.py
@@ -14,7 +14,9 @@
 
 from confluent_kafka import Consumer, KafkaException, KafkaError
 import sys
-
+import pymysql.cursors
+from datetime import date, datetime, timedelta
+import ast
 
 # 用來接收從Consumer instance發出的error訊息
 def error_cb(err):
@@ -42,19 +44,42 @@ if __name__ == '__main__':
     # Consumer configuration
     # See https://github.com/edenhill/librdkafka/blob/master/CONFIGURATION.md
     props = {
-        'bootstrap.servers': 'localhost:9092',       # Kafka集群在那裡? (置換成要連接的Kafka集群)
-        'group.id': 'STUDENT_ID',                     # ConsumerGroup的名稱 (置換成你/妳的學員ID)
-        'auto.offset.reset': 'earliest',             # Offset從最前面開始
+        'bootstrap.servers': '10.120.26.15:9092',       # Kafka集群在那裡? (置換成要連接的Kafka集群)
+        'group.id': '11',                     # ConsumerGroup的名稱 (置換成你/妳的學員ID)
+        'auto.offset.reset': 'earliest',             # Offset從最前面開始 latest從最新的開始
+        'enable.auto.commit': True,                 # 每次consume資料不再重抓
         'session.timeout.ms': 6000,                  # consumer超過6000ms沒有與kafka連線，會被認為掛掉了
         'error_cb': error_cb                         # 設定接收error訊息的callback函數
     }
+
+    # Connect to the database
+    connection = pymysql.connect(host='127.0.0.1',
+                                 port=3306,
+                                 user='root',
+                                 password='zxc2520',
+                                 db='kafka_consume',
+                                 charset='utf8mb4',
+                                 cursorclass=pymysql.cursors.DictCursor)
+
+    # Connect to the database 連接sql另一種寫法
+    # config ={
+    #     'host': '127.0.0.1',
+    #     'port': 3306,
+    #     'user': 'root',
+    #     'password': 'zxc2520',
+    #     'db': 'kafka_consume',
+    #     'charset': 'utf8',
+    #     'cursorclass': pymysql.cursors.DictCursor,
+    # }
+    # connection = pymysql.connect(config)
+
     # 步驟2. 產生一個Kafka的Consumer的實例
     consumer = Consumer(props)
     # 步驟3. 指定想要訂閱訊息的topic名稱
     topicName = "par12345"
 
     # 步驟4. 讓Consumer向Kafka集群訂閱指定的topic
-    consumer.subscribe([topicName], on_assign=my_assign)
+    consumer.subscribe([topicName], on_assign=my_assign) # on_assign=my_assign為從offset=0開始抓
     # 步驟5. 持續的拉取Kafka有進來的訊息
     count = 0
     try:
@@ -72,7 +97,7 @@ if __name__ == '__main__':
                     # Error or event
                     if record.error().code() == KafkaError._PARTITION_EOF:
                         # End of partition event
-                        sys.stderr.write('%% {} [{}] reached end at offset {} - {}\n'.format(record.topic(),
+                        sys.stderr.write('%% {} [{}] reached end at offset {} - {}'.format(record.topic(),
                                                                                              record.partition(),
                                                                                              record.offset()))
 
@@ -80,24 +105,41 @@ if __name__ == '__main__':
                         # Error
                         raise KafkaException(record.error())
                 else:
-                    # ** 在這裡進行商業邏輯與訊息處理 **
+                    # 這裡進行商業邏輯與訊息處理
                     # 取出相關的metadata
                     topic = record.topic()
                     partition = record.partition()
                     offset = record.offset()
                     timestamp = record.timestamp()
-                    # 取出msgKey與msgValue
+                    # 獲取時間
+                    time = str(datetime.now())[:10].replace("-","")
+                    # 取出msgKey與msgValue並轉utf8
                     msgKey = try_decode_utf8(record.key())
                     msgValue = try_decode_utf8(record.value())
 
+                    # msg_dict = ast.literal_eval(msgValue) 可將資料轉字典型態
                     # 秀出metadata與msgKey & msgValue訊息
                     count += 1
                     print('{}-{}-{} : ({} , {})'.format(topic, partition, offset, msgKey, msgValue))
-    except KeyboardInterrupt as e:
-        sys.stderr.write('Aborted by user\n')
+                # 執行sql語句
+                    try:
+                        with connection.cursor() as cursor:
+                            # 執行sql語句，插入資料
+                            print(msgKey, msgValue, time)
+
+                            cursor.execute("""INSERT INTO test1 VALUE ('{}','{}','{}')""".format(msgKey, msgValue, time))
+
+                            # sql = """INSERT INTO test1 (key, value, time) VALUES (%s, %s, %s)"""
+                            # cursor.execute(sql, ( msgKey, msgValue, time))
+                        # 沒有設置自動默認提交，需主動提交，以保存所執行的語句
+                        connection.commit()
+                    except Exception as e:
+                        print(e)
+
     except Exception as e:
         sys.stderr.write(e)
-
+        print(e)
     finally:
         # 步驟6.關掉Consumer實例的連線
+        connection.close()
         consumer.close()
